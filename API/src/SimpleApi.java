64509cc81cb1d3b12bdfbbf8baa0d1d8f8e5824a
YagoSanabria <yago.sanabria.gavin@gmail.com>
2025-03-12 11:31:20 +0100

    forecast init and renamed files

--- API/src/SimpleApi.java
+++ API/src/SimpleApi.java
@@ -1,3 +1,4 @@
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -6,16 +7,15 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
-
 public class SimpleApi {
 
     public static void main(String[] args) throws IOException {
 
         //Create http server that listens on port 8080
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        
+
         //Define handler for GET "/api"
-        server.createContext("/api", new HttpHandler() {
+        server.createContext("/api/", new HttpHandler() {
             @Override
             public void handle(HttpExchange exchange) throws IOException {
 
@@ -26,7 +26,7 @@ public class SimpleApi {
                 File file = new File("../db/" + city + ".json");
 
                 if (!file.exists()) {
-                    String response = "{message: \"City not found\"}";
+                    String response = "{Error message: \"" + city + " not found\"}";
                     exchange.sendResponseHeaders(404, response.getBytes().length);
                     OutputStream os = exchange.getResponseBody();
                     os.write(response.getBytes());
@@ -41,7 +41,35 @@ public class SimpleApi {
                 }
             }
         });
-        
+
+        //Define handler for GET "/forecast"
+        server.createContext("/forecast/", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+
+                String city = exchange.getRequestURI().toString().substring(9); //9 is /forecast length
+                System.out.println("\nCity name: " + city);
+
+                //check if city is in db
+                File file = new File("../db/" + city + ".json");
+
+                if (!file.exists()) {
+                    String response = "{Error message: \"" + city + " not found\"}";
+                    exchange.sendResponseHeaders(404, response.getBytes().length);
+                    OutputStream os = exchange.getResponseBody();
+                    os.write(response.getBytes());
+                    os.close();
+                } else {
+                    System.out.println("Send response from " + city);
+                    byte[] fileBytes = java.nio.file.Files.readAllBytes(file.toPath());
+                    exchange.sendResponseHeaders(200, fileBytes.length);
+                    OutputStream os = exchange.getResponseBody();
+                    os.write(fileBytes);
+                    os.close();
+                }
+            }
+        });
+
         //Start the server
         server.setExecutor(null);
         server.start();
--- weather-api/src/WeatherApi.java
+++ weather-api/src/WeatherApi.java
@@ -1,51 +1,106 @@
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.*;
+import java.net.*;
 import java.util.Scanner;
-import org.json.JSONObject;
-
+import org.json.*;
 
 public class WeatherApi {
 
     public static void main(String[] args) {
-
         final double KELVIN = 273.15;
-
-        String apiUrl = "http://localhost:8080/api"; //port 8080
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
+            String apiUrl = "http://localhost:8080/"; // port 8080
 
-            System.out.print("\nInsert city or country name (exit to quit): ");
-            String city = scanner.nextLine();
-            String direccion = "";
-
-            if (city.equals("exit")) {
-                break;
-            } else if (city.equals("")) {
+            System.out.print("\nCurrent weather (api) or forecast (forecast) [exit to quit]: ");
+            String type = scanner.nextLine().replaceAll(" ", "").toLowerCase();
+            if (type.equals("")) {
                 continue;
-            }else{
-                direccion = apiUrl+city.replaceAll(" ", "");
+            } else if (type.equals("exit")) {
+                break;
+            } else if (!type.equals("api") && !type.equals("forecast")) {
+                System.out.println("Invalid type. Try again.");
+                continue;
+            } else {
+                apiUrl += type + "/";
             }
 
+            System.out.println("New uri: " + apiUrl);
+            System.out.print("\nInsert city or country name [exit to quit]: ");
+            String city = scanner.nextLine().replaceAll(" ", "").toLowerCase();
+            if (city.equals("exit")) {
+                break;
+            } else if (city.equals("") || !city.matches("[a-z]+")) {
+                System.out.println("Invalid city or country name. Try again.");
+                continue;
+            }
+
+            String direccion = apiUrl + city;
             try {
-
                 URL url = new URL(direccion);
-
-                //Open http connection
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 connection.setRequestMethod("GET");
-                connection.setConnectTimeout(5000);  //Connection timeout of 5 seconds
-                connection.setReadTimeout(5000);  //Read timeout of 5 seconds
-
-                //Get http response code
+                connection.setConnectTimeout(5000);
+                connection.setReadTimeout(5000);
                 int status = connection.getResponseCode();
 
-                //If response code is 200, read the response (all ok)
                 if (status == 200) {
                     BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String inputLine;
+                    StringBuffer response = new StringBuffer();
+                    while ((inputLine = in.readLine()) != null) {
+                        response.append(inputLine);
+                    }
+                    in.close();
+
+                    JSONObject jsonResponse = new JSONObject(response.toString());
+                    if (type.equals("api")) {
+                        String cityName = jsonResponse.getString("name");
+                        String country = jsonResponse.getJSONObject("sys").getString("country");
+                        double temperature = jsonResponse.getJSONObject("main").getDouble("temp") - KELVIN;
+                        double tempLike = jsonResponse.getJSONObject("main").getDouble("feels_like") - KELVIN;
+                        double humidity = jsonResponse.getJSONObject("main").getDouble("humidity");
+                        String weatherDescription = jsonResponse.getJSONArray("weather").getJSONObject(0).getString("main") + ", " + jsonResponse.getJSONArray("weather").getJSONObject(0).getString("description");
+                        double coordX = jsonResponse.getJSONObject("coord").getDouble("lat");
+                        double coordY = jsonResponse.getJSONObject("coord").getDouble("lon");
+                        String coordXtxt = (coordX < 0) ? Math.abs(coordX) + "S" : coordX + "N";
+                        String coordYtxt = (coordY < 0) ? Math.abs(coordY) + "W" : coordY + "E";
+
+                        System.out.println("\nWeather in " + cityName + "(" + country + "), (" + coordXtxt + "," + coordYtxt + "):");
+                        System.out.println("\tDescription: " + weatherDescription);
+                        System.out.println("\tTemperature: " + String.format("%.2f", temperature) + "°C, feels like " + String.format("%.2f", tempLike) + "°C");
+                        System.out.println("\tHumidity: " + humidity + "%");
+                    } else if (type.equals("forecast")) {
+
+                        /*
+                        System.out.println("Weather Forecast for " + jsonResponse.getJSONObject("city").getString("name") + ", " + jsonResponse.getJSONObject("city").getString("country"));
+                        System.out.println("----------------------------------------------------");
+                        JSONArray forecasts = jsonResponse.getJSONArray("list");
+                        for (int i = 0; i < forecasts.length(); i++) {
+                            JSONObject forecast = forecasts.getJSONObject(i);
+                            String dateTime = forecast.getString("dt_txt");
+                            double temperature = forecast.getJSONObject("main").getDouble("temp") - KELVIN;
+                            double feelsLike = forecast.getJSONObject("main").getDouble("feels_like") - KELVIN;
+                            String weatherMain = forecast.getJSONArray("weather").getJSONObject(0).getString("main");
+                            String weatherDesc = forecast.getJSONArray("weather").getJSONObject(0).getString("description");
+                            double windSpeed = forecast.getJSONObject("wind").getDouble("speed");
+                            System.out.println("Date/Time: " + dateTime);
+                            System.out.println("Temperature: " + String.format("%.2f", temperature) + "°C");
+                            System.out.println("Feels Like: " + String.format("%.2f", feelsLike) + "°C");
+                            System.out.println("Weather: " + weatherMain + " - " + weatherDesc);
+                            System.out.println("Wind Speed: " + windSpeed + " m/s");
+                            System.out.println("----------------------------------------------------");
+                        }
+                         */
+                    }
+                } else {
+                    System.out.println("Error in request. Response code: " + status);
+
+                    //Get error message
+                    String a = connection.getResponseMessage();
+
+                    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
                     String inputLine;
                     StringBuffer response = new StringBuffer();
 
@@ -54,47 +109,19 @@ public class WeatherApi {
                     }
                     in.close();
 
-                    //Parse response to json
                     JSONObject jsonResponse = new JSONObject(response.toString());
-
-                    ///Get data from json
-                    String cityName = jsonResponse.getString("name");
-                    String country = jsonResponse.getJSONObject("sys").getString("country");
-                    double temperature = jsonResponse.getJSONObject("main").getDouble("temp") - KELVIN;
-
-                    double tempLike = jsonResponse.getJSONObject("main").getDouble("feels_like")- KELVIN;
-
-                    double humidity = jsonResponse.getJSONObject("main").getDouble("humidity");
-                    String weatherDescription = jsonResponse.getJSONArray("weather").getJSONObject(0).getString("main") + ", " + jsonResponse.getJSONArray("weather").getJSONObject(0).getString("description");
-
-                    double coordX = jsonResponse.getJSONObject("coord").getDouble("lat");
-                    double coordY = jsonResponse.getJSONObject("coord").getDouble("lon");
-
-                    String coordXtxt = (coordX < 0) ? Math.abs(coordX) + "S" : coordX + "N";
-                    String coordYtxt = (coordY < 0) ? Math.abs(coordY) + "W" : coordY + "E";
-
-                    //Print data in terminal
-                    System.out.println("\nWeather in " + cityName + "(" + country + "), " + "(" + coordXtxt + "," + coordYtxt + "):");
-                    System.out.println("\tDescription: " + weatherDescription);
-                    System.out.println("\tTemperature: " + (String.format("%.2f", temperature)) + "°C, feels like " + (String.format("%.2f", tempLike)) + "°C");
-                    System.out.println("\tHumidity: " + humidity + "%");
-
-
-                } else {
-                    System.out.println("Error in request. Response code: " + status);
-
-                    //Get error message
+                    String msg = jsonResponse.getString("Error message");
+                    System.out.println("\tError message: " + msg);
                 }
 
                 //Close connection
                 connection.disconnect();
 
             } catch (Exception e) {
-                e.printStackTrace();
+                System.out.println("Error. Response code: 503");
+                System.out.println("Trying to request to server");
             }
         }
-
-        //exit
         scanner.close();
         System.out.println("Exiting program...");
     }
